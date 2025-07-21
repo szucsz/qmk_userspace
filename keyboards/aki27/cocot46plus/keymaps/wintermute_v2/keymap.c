b649caa59ed9bc694e2c1f96b92ffa89bd31652f
Zoltan Szucs <zz.szucs@gmail.com>
2025-07-21 07:48:49 +0000

    fix: windows gui keys

--- keyboards/aki27/cocot46plus/keymaps/wintermute_v2/keymap.c
+++ keyboards/aki27/cocot46plus/keymaps/wintermute_v2/keymap.c
@@ -52,7 +52,7 @@ enum custom_keycodes {
 #define HS(k)     MT(MOD_LSFT,k)
 #define HC(k)     MT(MOD_LCTL,k)
 #define HA(k)     MT(MOD_LALT,k)
-#define HG(k)     MT(MOD_LGUI,k)
+#define HG(k)     MT(MOD_RGUI,k)
 
 #define CA(k)     C(A(k))
 #define SA(k)     S(A(k))
@@ -61,7 +61,6 @@ enum custom_keycodes {
 #define GC(k)     G(C(k))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  //`------' O O O     O O O `------'
   //`------' O O O     O O O `------'
   [L_MB] = LAYOUT(
   //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
@@ -79,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_LCTL,  HG(KC_A),  HA(KC_S),   HC(KC_D),   HS(KC_F),   KC_G,                 KC_H,     HS(KC_J),   HC(KC_K),   HA(KC_L),   HG(KC_SCLN), KC_QUOT,
        KC_LSFT,     KC_Z,      KC_X,       KC_C,       KC_V,    KC_B,                 KC_N,        KC_M,    KC_COMM,     KC_DOT,       KC_SLSH, KC_BSLS,
   //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
-                  XXXXXXX, KC_LALT, KC_TAB, WF(KC_BSPC), KC_MS_BTN1,                 KC_MS_BTN2,  WS(KC_SPC), KC_DEL,   KC_LCTL, TG(L_CURSOR),
+                  XXXXXXX, KC_LALT, KC_TAB, WF(KC_BSPC), KC_MS_BTN1,                 KC_MS_BTN2,  WS(KC_SPC), KC_DEL,   KC_RGUI, TG(L_CURSOR),
                                                                  XXXXXXX, DF(L_MB), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
                                //`------------------------------------' O O O  O O O `------------------------------------'
   ),
@@ -90,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
       _______,   C(KC_GRV),  KC_HOME,   KC_UP,      KC_END,     KC_DEL,                      KC_F1,      KC_F2,    KC_F3,      KC_F4,      KC_F5,    KC_F6,
        W_NEXT_W, W_PREV_W,   KC_LEFT,   KC_DOWN,    KC_RGHT,    KC_PGUP,                     KC_F7,   HS(KC_F8),  HC(KC_F9),  HA(KC_F10), HG(KC_F11),  KC_F12,
-      CW_TOGG,     C(KC_Z),  C(KC_X),   C(KC_C),    C(KC_V),    KC_PGDN,                     KC_INS,   C(KC_W),    C(KC_PGUP), C(KC_PGDN), C(KC_F5), C(KC_F12),
+      CW_TOGG,     C(KC_Z),  C(KC_X),   C(KC_C),    C(KC_V),    KC_PGDN,                     KC_INS,   C(KC_W),    C(KC_PGUP), C(KC_PGDN), C(KC_F11), C(KC_F12),
    //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
                               _______, KC_VOLD, KC_VOLU, KC_BSPC, KC_MS_BTN1,         KC_MS_BTN2,  KC_SPC, _______,  _______, _______,
                                                                      XXXXXXX, TG(L_WF) , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -100,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
       _______,   C(KC_GRV),G(KC_LEFT),   KC_UP,  G(KC_RIGHT),     KC_DEL,                      KC_F1,      KC_F2,    KC_F3,     KC_F4,         KC_F5,    KC_F6,
       M_NEXT_W,  M_PREV_W,   KC_LEFT,   KC_DOWN,    KC_RGHT,    KC_PGUP,                     KC_F7,   HS(KC_F8), HG(KC_F9), HA(KC_F10), HC(KC_F11),   KC_F12,
-      CW_TOGG,     G(KC_Z),  G(KC_X),   G(KC_C),    G(KC_V),    KC_PGDN,                     KC_INS,   G(KC_W), SG(KC_LCBR), SG(KC_RCBR),   C(KC_F5), SG(KC_4),
+      CW_TOGG,     G(KC_Z),  G(KC_X),   G(KC_C),    G(KC_V),    KC_PGDN,                     KC_INS,   G(KC_W), SG(KC_LCBR), SG(KC_RCBR),   C(KC_F11), SG(KC_4),
    //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
                               _______, KC_VOLD, KC_VOLU, KC_BSPC, KC_MS_BTN1,         KC_MS_BTN2,  KC_SPC, _______,  _______, _______,
                                                                      XXXXXXX, TG(L_MF) , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -112,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
         _______, KC_EXLM,     KC_AT,    KC_HASH,  KC_DLR,  KC_PERC,                     KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_COMM,
         _______, HG(KC_1), HA(KC_2),   HC(KC_3), HS(KC_4),   KC_5,                     KC_6,    HS(KC_7), HC(KC_8), HA(KC_9), HG(KC_0),  KC_DOT,
-        _______, XXXXXXX,     KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR,                     KC_UNDS,  KC_MINS,   KC_EQL,  KC_PLUS,  KC_SLSH,  XXXXXXX,
+        _______, XXXXXXX,     KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR,                     KC_UNDS,  KC_MINS,   KC_EQL,  KC_PLUS,  KC_SLSH,  S(KC_SCLN),
     //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
                               _______, _______, _______, KC_BSPC, KC_MS_BTN1,         KC_MS_BTN2,  KC_SPC, _______,  _______, _______,
                                                            XXXXXXX, TG(L_WS),         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
@@ -121,8 +120,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [L_MS] = LAYOUT(
     //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
         _______, KC_EXLM,     KC_AT,    KC_HASH,  KC_DLR,  KC_PERC,                     KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  KC_COMM,
-        _______, HC(KC_1), HA(KC_2),   HG(KC_3),HS(KC_4),   KC_5,                        KC_6,    HS(KC_7), HG(KC_8), HA(KC_9), HC(KC_0),  KC_DOT,
-        _______, XXXXXXX,     KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR,                     KC_UNDS,  KC_MINS,   KC_EQL,  KC_PLUS,  KC_SLSH,  XXXXXXX,
+        _______, HC(KC_1), HA(KC_2),   HG(KC_3),HS(KC_4),   KC_5,                        KC_6,    HS(KC_7), HG(KC_8), HA(KC_9), HC(KC_0), KC_DOT,
+        _______, XXXXXXX,     KC_LBRC,  KC_RBRC, KC_LCBR, KC_RCBR,                      KC_UNDS,  KC_MINS,   KC_EQL,  KC_PLUS,  KC_SLSH,  S(KC_SCLN),
     //|------------------------------------------------------------------|            |------------------------------------------------------------------------|
                               _______, _______, _______, KC_BSPC, KC_MS_BTN1,         KC_MS_BTN2,  KC_SPC, _______,  _______, _______,
                                                            XXXXXXX, TG(L_MS),         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
