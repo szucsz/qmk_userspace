13ad91bba99af4f6111b0601f37bbefbd86e1cd1
Zoltan Szucs <zz.szucs@gmail.com>
2025-08-04 18:43:29 +0000

    change tapping term to 200

--- keyboards/aki27/cocot46plus/keymaps/wintermute_v2/config.h
+++ keyboards/aki27/cocot46plus/keymaps/wintermute_v2/config.h
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SLEEP
 #define RGBLIGHT_DEFAULT_VAL 0
 
-#define TAPPING_TERM 300
+#define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
 
 #ifdef DYNAMIC_KEYMAP_LAYER_COUNT
